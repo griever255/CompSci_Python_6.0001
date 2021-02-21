23679b5b9056bf8af825ff4008358f3e26032191
Joe <jgargiulo95@gmail.com>
2021-02-20 19:57:26 -0500

    Completed Problem Set #1

--- Lec3/ps1c.py
+++ Lec3/ps1c.py
@@ -1,27 +1,45 @@
-# Calculate how long it will take to save for a
+## Calculate how long it will take to save for a
 # down payment given the following assumptions:
 
-# User inputs
+## User input
 annual_salary = int(input("Enter your annual salary: ")) # Annual salary
+
+## Given assumptions
 total_cost =  1000000 # Cost of your dream home
 semi_annual_raise = 0.07 # Raise every 6 months (e.g 0.03 = 3%)
-
-# Given assumptions
 portion_down_payment = 0.25 # Down payment (e.g. 0.25 = 25%)
-current_savings = 0 # Current savings
 r = 0.04 # 4% rate of return on the current savings
+current_savings = 0
 
-# Calculations
-monthly_salary = annual_salary/12
-monthly_savings = monthly_salary*portion_saved
+## Bisection Search
+epsilon = 100 # Within $100 of required down payment
+low = 0
+high = 10000
+portion_saved = (high+low)/2.0
+numSteps = 0
+notPossible = False
+while abs(current_savings - total_cost*portion_down_payment) >= epsilon:
+    current_savings = 0
+    monthly_salary = annual_salary/12
+    monthly_savings = monthly_salary*portion_saved/10000
+    for month in range(36):
+        current_savings += current_savings*r/12 + monthly_savings
+        if month % 6 == 0 and month != 0:
+            monthly_salary *= 1+semi_annual_raise
+            monthly_savings = monthly_salary*portion_saved/10000
+    if current_savings > total_cost*portion_down_payment:
+        high = portion_saved
+    else:
+        low = portion_saved
+    portion_saved = round((high+low)/2.0,0)
+    if portion_saved == 10000:
+        notPossible = True
+        break
+    numSteps += 1
 
-
-for i in range(36) current_savings < total_cost*portion_down_payment:
-    current_savings += current_savings*r/12 + monthly_savings
-    if months % 6 == 0 and months != 0:
-        monthly_salary *= 1+semi_annual_raise
-        monthly_savings = monthly_salary*portion_saved
-    months += 1
-
-print("Number of months:", months)
+if notPossible:
+    print("It is not possible to pay the down payment in three years.")
+else:
+    print("Best savings rate:", round(portion_saved/10000,4))
+    print("Steps in bisection search:", numSteps)
 
